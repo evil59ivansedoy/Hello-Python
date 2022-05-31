3e7e8ad11d105884ea7e32ead0589f6a6cc851d3
Тафейко Иван <ivav1989ritser@icloud.com>
2022-05-31 20:05:19 +0300

    week_day

--- tack01/tack1.py
+++ tack01/tack1.py
@@ -1,1 +1,7 @@
-print('Hello world')
+#Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+def week_day():
+    num_day = int(input('введите номер дня'))
+    if num_day >= 1 and num_day <= 5 : return ('no')
+    elif num_day == 6 or 7  : return('yes')
+print(week_day())
+
